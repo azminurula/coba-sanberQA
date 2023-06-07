f13bb7c3aa759a98987aac3a124f22e77f8f5860
azminurula <azminurulana@gmail.com>
2023-06-07 10:59:49 +0700

    revisi login baru

--- revisi login.js
+++ revisi login.js
@@ -1,3 +1,3 @@
 export default function {
-    console.log("Ini adalah revisi fitur login baru")
+    console.log("Ini adalah revisi fitur login baru, dengan ada nya perbaikan dibagian input password dan email")
 }
